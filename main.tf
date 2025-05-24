a2a63d149d87d23355236d1e4168c8c546ae60c2
Mahmoud Kamal <53492856+MahmoudKamal01@users.noreply.github.com>
2025-05-24 15:36:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-084568db4383264d4" # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name      = "jenk.pem"        # Must exist in AWS
+
+  tags = {
+    Name = "Jenkins-Terraform-Instance"
+  }
+}
