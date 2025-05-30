90cb52eb703c40883902486acab7c810907f8e06
Mahmoud Kamal <53492856+MahmoudKamal01@users.noreply.github.com>
2025-05-30 22:21:26 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example" {
   ami           = "ami-084568db4383264d4" # Amazon Linux 2
   instance_type = "t2.micro"
   key_name      = "jenk.pem"        # Must exist in AWS
-
+  subnet_id     = "subnet-0248e204859587f21"
   tags = {
     Name = "Jenkins-Terraform-Instance"
   }
